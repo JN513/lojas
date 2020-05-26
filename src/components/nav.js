a006563eaa5f3dd0cf3ffd654d8bd60c93f70be8
BrunoSan123 <Bruno-kk@hotmail.com>
2020-05-26 09:58:22 -0300

    setup inicial da loja

---
+++ src/components/nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {FaStore} from 'react-icons/fa'
+import './nav.css'
+
+
+function Nav(){
+
+    return(
+   
+      <div className="container">
+          <span className="icon">
+            
+          </span>
+
+          
+
+      </div>
+       
+   );
+}
+
+export default Nav;
